1b05f43a11a7097c782477c7e30b05dae2c2105b
shintak777 <hiraking@live.jp>
2021-06-02 18:27:39 +0900

    Reactでポートフォリオ制作

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ul>
+       <li>
+         信じていた信じられたその全てを今始めよう</li>
+         <li>不器用なこの勇気はコンプレックス・イマージュ</li>
+         <li>乾いた風が追いかける波のように</li>
+         <li>そっと背中を押す気がした</li>
+         <li>耳を澄ませばざわめくロンフリーケンシー</li>
+         <li>新しいその場所への扉を開け放て</li>
+         <li>切なくて愛おしいほど想いは時空を超えて</li>
+         <li>together</li>
+         <li>ずっと心はそばにある</li>
+         <li>大切なあの日だまりが</li>
+         <li>刹那に散りゆく景色</li>
+         <li>今全ての力を与え賜え</li>
+         <li>僕は瞳をそらさずに</li>
+         <li>傷も涙も真実も</li>
+
+     </ul>
     </div>
   );
 }
