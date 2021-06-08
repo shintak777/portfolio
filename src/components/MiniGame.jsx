e59b114f8983d6db6cdfcbcddcc9c0e1f164182f
shintak777 <hiraking@live.jp>
2021-06-08 15:32:27 +0900

    パチ完成まであと少し

--- src/components/MiniGame.jsx
+++ src/components/MiniGame.jsx
@@ -2,28 +2,23 @@ import React, { useState, Component } from 'react';
 import '../assets/css/MiniGame.css';
 
 const MiniGame = () => {
-  const btn = document.getElementById('btn');
-
+  const btn = document.getElementById("btn");
+  const Bonus =[
+    '111', '222', '333', '444', '555', '666', '777'
+  ];
   const WhenTheyCry =[
     '121', '232', '343', '454', '565', '676'
   ];
 
-  btn?.addEventListener('click', function(){
-    const n = Math.random()
-    if(n < 
-      0.12987012987012986){
-      this.textContent = '惨劇回避';
+  btn?.addEventListener("click", () => {
+    const n = Math.random();
+    if(n < 0.12987012987012986){
+      btn.textContent = Bonus[Math.floor(Math.random()*7)];
     }else{
-      this.textContent = WhenTheyCry[Math.floor(Math.random()*6)];
+      btn.textContent = WhenTheyCry[Math.floor(Math.random()*6)];
     }
 
   });
-  btn.addEventListener('mousedown', function(){
-    this.className = 'pushed';
-  });
-  btn.addEventListener('mouseup', function(){
-    this.className = '';
-  })();
 
 return (
   <div>
@@ -31,7 +26,7 @@ return (
     <h2 id="MiniGame">MiniGame</h2>
       <div id = "btn">PUSH</div>
       <ul className="delete-marker">
-        <li>1/7.7の確率で当たります"</li>
+        <li>1/7.7の確率で当たります</li>
           <li>脳汁を出したいときなど暇つぶしにどうぞ。</li>
       </ul>
     </div>
